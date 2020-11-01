5ee5cd2a139e69c450e45d3914afe4210f2853ce
Ton Nattaphon <ton.edu63@gmail.com>
2020-11-01 19:54:03 +0700

    linkpage

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import './App.css';
 import Header from './components/Header';
 import TinderCards from './components/TinderCards';
 import SwipeButtons from './components/SwipeButtons'
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      
-      <Header />
-      <TinderCards />
-      <SwipeButtons />
+      <Router>
+        <Switch>
 
+            <Route path="/chat">
+              <Header backButton="/"/>
+              <h1>I am chat page</h1>
+            </Route>
+
+            <Route path="/setting">
+              <Header backPage="/"/>
+              <h1>I am setting page</h1>
+            </Route>
+
+            <Route path ="/" >
+              <Header />
+              <TinderCards />
+              <SwipeButtons />
+            </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,47 @@ import './Header.css'
 import PersonIcon from '@material-ui/icons/Person'; 
 import IconButton from '@material-ui/core/IconButton';
 import ForumIcon from '@material-ui/icons/Forum';
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import { Link, useHistory } from 'react-router-dom';
 
-function Header() {
+function Header({ backButton, backPage }) {
+    const history = useHistory();
     return (
         <div className="header">
-            
-            <IconButton>
-               <PersonIcon fontSize="large" className="header_icon"/>
-            </IconButton>
-            
+            { backButton ? (
+                <IconButton onClick = {() => history.replace(backButton)}>
+                  <ArrowBackIosIcon fontSize="large" className="header_icon"/>
+                </IconButton>
+            ):(
+                <Link to="/setting">
+                  <IconButton>
+                    <PersonIcon fontSize="large" className="header_icon"/>
+                  </IconButton>
+                </Link>
+            )}
+            <Link to="/">
             <img
                 className="header_logo"
                 src="https://www.vectorico.com/wp-content/uploads/2018/02/Tinder-Logo-506x458.png"
                 alt=""
             
             />
+            </Link>
+            { backPage ? (
+                <IconButton onClick = {() => history.replace(backPage)}>
+                  <ArrowForwardIosIcon fontSize="large" className="header_icon"/>
+                </IconButton>
+            ):(
+                <Link to="/chat">
             <IconButton>
                 <ForumIcon fontSize="large" className="header_icon"/>
             </IconButton>
+            </Link>
 
+            )}
+            
+            
         </div>
     )
 }
