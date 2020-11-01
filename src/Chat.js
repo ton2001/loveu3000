52f22cb4fb77e1680a33101595b327860be26776
maxpeeranut <stapler.mag@gmail.com>
2020-11-01 19:57:28 +0700

    chat

---
+++ src/Chat.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Chat.css";
+import Avatar from "@material-ui/core/Avatar"
+import { Link } from "react-router-dom";
+
+function Chat({name, message, profilePic, timestamp})
+{
+    return (
+        <Link to={'/chat/${name}'}>
+        <div className="chat">
+            <Avatar className="chat_image" alt={name} src={profilePic}></Avatar>
+            <div className="chat_details">
+                <h2>{name}</h2>
+                <p>{message}</p>
+            </div>
+            <p className="chat_timestamp">{timestamp}</p>     
+        </div>
+        </Link>
+    );
+}
+
+export default Chat;
---
+++ src/ChatScreen.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Avatar from "@material-ui/core/Avatar";
+import "./ChatScreen.css";
+
+function ChatScreen()
+{
+    const [messages, setMessages] = useState([
+        {
+            name: 'Ellen',
+            image: '',
+            message: 'Whats up',
+        },
+        {
+            name: 'Ellen',
+            image: '',
+            message: 'Whats up',
+        },
+        {
+            message: 'Whats up',
+        },
+    ]);
+
+    return(
+        <div className="chatScreen">
+            <h2>Chat screen</h2>
+            <p className="chatScreen_timestamp">
+                YOU MATCHED WITH SASSADA ON 10/08/2020
+            </p>
+            message.name ? (
+                <div className="chatScreen_message">
+                    <Avatar
+                        className="chatScreen_image"
+                        alt={message.name}
+                        src={message.image}
+                        />
+                        <p className="chatScreen_text">{message.message}</p>
+                </div>
+                )   :   (
+                    <div className="chatScree_message">
+                        <p className="chatScreen_text"{message.message}></p>
+                    </div>
+                )
+            }
+        </div>
+    );
+}
+
+export default ChatScreen;
